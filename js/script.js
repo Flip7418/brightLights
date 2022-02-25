c5267f9067181926de67ba94baec74534acab603
180107062 <62544970+Flip7418@users.noreply.github.com>
2022-02-25 23:01:45 +0600

    25.02.2022

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
     $(".header__burger,.header__menu,.header").toggleClass("active");
-    $('body').toggleClass("lock");
+    $("body").toggleClass("lock");
+  });
+});
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    slidesToShow: 3,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    pauseOnFocus: true,
+    dragable: false,
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 1087,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    mobileFist: false,
   });
 });
